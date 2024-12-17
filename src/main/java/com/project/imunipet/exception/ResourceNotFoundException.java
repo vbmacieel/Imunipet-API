5f010b38973c2b4810ad140b243f7ce3350b6621
Victor Bastos <vbastosmaciel@outlook.com.br>
2024-12-16 23:23:28 -0300

    feat: Adding new type supported for id on exception

--- src/main/java/com/project/imunipet/exception/ResourceNotFoundException.java
+++ src/main/java/com/project/imunipet/exception/ResourceNotFoundException.java
@@ -5,4 +5,8 @@ public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(Long id) {
         super("Entity not found for id: " + id);
     }   
+
+    public ResourceNotFoundException(String id) {
+        super("Entity not found for id: " + id);
+    }   
 }

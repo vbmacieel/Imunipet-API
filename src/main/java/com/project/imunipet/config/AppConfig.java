3f16a6aca5c95166ebd662a7be0805c0458a866c
Victor Bastos <vbastosmaciel@outlook.com.br>
2024-12-16 23:22:33 -0300

    fix: Fix bean method

--- src/main/java/com/project/imunipet/config/AppConfig.java
+++ src/main/java/com/project/imunipet/config/AppConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
 
     @Bean
-    public ModelMapper modelMapper() {
+    ModelMapper modelMapper() {
         return new ModelMapper();
     }
 }
